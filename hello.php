fed71fd72af39344ca3ca54055c262dd458348ba
Fredrik Steen <fredrik.steen@redpill-linpro.com>
2017-05-29 11:44:31 +0200

    cleanup

--- hello.php
+++ hello.php
@@ -19,27 +19,5 @@
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
-	<?php
-	$links = [];
-	foreach($_ENV as $key => $value) {
-		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-			$links[] = [
-				"name" => $matches[1],
-				"port" => $matches[2],
-				"proto" => $matches[3],
-				"value" => $value
-			];
-		}
-	}
-	if($links) {
-	?>
-		<h3>Links found</h3>
-		<?php
-		foreach($links as $link) {
-			?>
-			<b><?php echo $link["name"]; ?></b> listening in <?php echo $link["port"]+"/"+$link["proto"]; ?> available at <?php echo $link["value"]; ?><br />
-			<?php
-		}
-		?>
 </body>
 </html>
